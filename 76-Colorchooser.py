1badb7f53856a965e4cbb236afb8d49e83bee704
Bill-Corkery <wbcorkery@gmail.com>
2022-09-06 19:38:13 -0400

    77-Text Area

--- 76-Colorchooser.py
+++ 76-Colorchooser.py
@@ -1,2 +1,17 @@
 #Lesson 79: Color chooser
 # https://www.youtube.com/watch?v=xiUTqnI6xk8
+
+from tkinter import *
+from tkinter import colorchooser  #submodule. not necessary.
+
+def click():
+#    window.config(bg=colorchooser.askcolor()[1])  #one line of code version of below.
+    color = colorchooser.askcolor() #assign color to a variable
+    color_Hex = color[1]            #assigns element at index 1 to a varaible
+    window.config(bg=color_Hex)     #change background color
+
+window = Tk()
+window.geometry('420x420')
+button = Button(text='click me', command=click)
+button.pack()
+window.mainloop()
--- 77-Text Area.py
+++ 77-Text Area.py
@@ -1,2 +1,23 @@
 #Lesson 77: Text Area
 # https://www.youtube.com/watch?v=xiUTqnI6xk8
+# text widget = functions like a text area, you can enter multiple lines of text
+
+from tkinter import *
+
+def submit():
+    input = text.get("1.0", END)
+    print(input)
+
+window = Tk()
+text = Text(window,
+            bg="light yellow",
+            font=("Ink Free",16),
+            height=8,
+            width=20,
+            padx=20,
+            pady=20,
+            fg="blue")
+text.pack()
+button = Button(window,text="submit", command=submit)
+button.pack()
+window.mainloop()

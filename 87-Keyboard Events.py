4459a95464d5d29a1b59f969f494c1951294a222
Bill-Corkery <wbcorkery@gmail.com>
2022-09-27 18:19:24 -0400

    87 Keyboard events

--- 87-Keyboard Events.py
+++ 87-Keyboard Events.py
@@ -1,2 +1,19 @@
 #Lesson 87: Keyboard Events
 # https://www.youtube.com/watch?v=xiUTqnI6xk8
+
+from tkinter import  *
+
+def doSomething(event):
+    print("You pressed:" + event.keysym)
+    label.config(text=event.keysym)
+
+window = Tk()
+
+# return is enter. Key is any key.
+window.bind("<Return>", doSomething)
+window.bind("<Key>", doSomething)
+
+label = Label(window,font=('Helvetica', 100))
+label.pack()
+
+window.mainloop()

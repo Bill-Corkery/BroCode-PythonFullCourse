4cf2c419e8b305b656e9449f0b9e1cb69d9c110c
Bill-Corkery <wbcorkery@gmail.com>
2022-10-03 16:06:41 -0400

    95 and 96

--- 95-Run with command prompt.py
+++ 95-Run with command prompt.py
@@ -1,2 +1,14 @@
 #Lesson 95: Run with command prompt
 # https://www.youtube.com/watch?v=xiUTqnI6xk8
+
+# ******************
+# run .py file with cmd
+# save file as .py (Python file)
+# go to command prompt
+# navigate to directory with your file: C:\Users\WilliamBillCorkery\PycharmProjects\BroCode-PythonFullCourse
+# invole python interpreter + script: python "95-Run with command prompt.py"
+
+print("Hello World!")
+
+name = input('What is your name?:  ')
+print("Hello "+name)
--- 96-Pip.py
+++ 96-Pip.py
@@ -1,2 +1,17 @@
 #Lesson 96: Pip
 # https://www.youtube.com/watch?v=xiUTqnI6xk8
+
+# ************************************
+# Python pip
+# ************************************
+# pip = package manager for packages and modules from Python Package index
+# Python package index is pypi.org. full list of packages.
+#
+# included for Python versions 3.4+
+# open command prompt
+#   help:                       pip
+#   check:                      pip --version
+#   update:                     pip install --upgrade pip
+#   installed packages:         pip list
+#   check outdated packages:    pip list --outdated
+#   install a package:          pip install "package name"

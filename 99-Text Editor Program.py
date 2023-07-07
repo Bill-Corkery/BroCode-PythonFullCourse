ba40e72baa034403bb80cee73c44d8895a8ae1ba
Bill-Corkery <wbcorkery@gmail.com>
2023-07-06 20:17:19 -0400

    finished 99-text editor program

--- 99-Text Editor Program.py
+++ 99-Text Editor Program.py
@@ -6,7 +6,6 @@ from tkinter import *
 from tkinter import filedialog, colorchooser, font
 from tkinter.messagebox import *
 from tkinter.filedialog import *
-# color chooser, font dont seem to be working from tkinter
 
 def change_color():
     color = colorchooser.askcolor(title="pick a color")
@@ -16,32 +15,70 @@ def change_font(*args):
     text_area.config(font=(font_name.get(), size_box.get()))
 
 def new_file():
-    pass
+    window.title("Untitled")
+    text_area.delete(1.0,END)
+
+
+# This original code below did not work. Error was "_tkinter.TclError: bad option "-file": must be -defaultextension, -filetypes, -initialdir, 
+    # file = askopenfilename(defaultextension=".txt",file=[("All Files", "*.*"),("Text Documents", "*.txt")])
+#replaced with this. Using this website. https://stackoverflow.com/questions/51071984/tkinter-askopenfilename-wont-open
+    # file = filedialog.askopenfilename(title="Open Filename",filetypes=(("TXT Files","*.txt"),("All Files","*.*")))
 
 def open_file():
-    pass
+    file = filedialog.askopenfilename(title="Open Filename",filetypes=(("TXT Files","*.txt"),("All Files","*.*")))
+
+    if file is None:
+        return
+    else:
+        try:
+            window.title(os.path.basename(file))
+            text_area.delete(1.0, END)
+        
+            file = open(file, "r")
+        
+            text_area.insert(1.0,file.read())
+        # better to name specific exceptions
+        except Exception:
+            print("Could not read file")
+        finally: 
+            file.close() 
 
 def save_file():
-    pass
+    file = filedialog.asksaveasfilename(initialfile= "untitled.txt", defaultextension=".txt", 
+                                        filetypes=[("All Files", "*.*"), 
+                                                   ("Text Document", "*.txt")])
+
+    if file is None:
+        return
+    else:
+        try:
+            window.title(os.path.basename(file))
+            file = open(file, "w")
+            file.write(text_area.get(1.0, END))
+        except Exception:
+            print("Could not save file")
+        finally:
+            file.close()
 
 def cut():
-    pass
+    text_area.event_generate("<<Cut>>")
 
 def copy():
-    pass
+    text_area.event_generate("<<Copy>>")
 
 def paste():
-    pass
+    text_area.event_generate("<<Paste>>")
 
 def about():
-    pass
+    showinfo("About this program", "This is a program written by Billy!!!")
 
 def quit():
-    pass
+    window.destroy()
 
 window = Tk()
 window.title("Text Editor Program")
 file = None
+
 window_width = 500
 window_height = 500
 screen_width = window.winfo_screenwidth()
@@ -64,6 +101,8 @@ scroll_bar = Scrollbar(text_area)
 window.grid_rowconfigure(0,weight=1)
 window.grid_columnconfigure(0,weight=1)
 text_area.grid(sticky=N + E + S + W)
+scroll_bar.pack(side=RIGHT, fill=Y)
+text_area.config(yscrollcommand=scroll_bar.set)
 
 frame = Frame(window)
 frame.grid()
@@ -71,13 +110,33 @@ frame.grid()
 color_button = Button(frame, text = "color", command=change_color)
 color_button.grid(row=0, column=0)
 
-font_box = OptionMenu(frame,font_name, *font.families(), command=change_font())
+font_box = OptionMenu(frame,font_name, *font.families(), command=change_font)
 font_box.grid(row=0, column=1)
 
-size_box = Spinbox(frame, from_=1,to=100, textvariable=font_size, command=change_font())
+size_box = Spinbox(frame, from_=1,to=100, textvariable=font_size, command=change_font)
 size_box.grid(row=0, column=2)
 
-scroll_bar.pack(side=RIGHT, fill=Y)
-text_area.config(yscrollcommand=scroll_bar.set)
+menu_bar = Menu(window)
+window.config(menu = menu_bar)
+
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label = "File", menu=file_menu)
+file_menu.add_command(label = "New", command = new_file)
+file_menu.add_command(label = "Open", command = open_file)
+file_menu.add_command(label = "Save", command = save_file)
+file_menu.add_separator()
+file_menu.add_command(label = "Exit", command = quit)
+
+edit_menu = Menu(menu_bar, tearoff = 0)
+menu_bar.add_cascade(label = "Edit", menu=edit_menu)
+edit_menu.add_command(label="Cut", command=cut)
+edit_menu.add_command(label="Copy", command=copy)
+edit_menu.add_command(label="Paste", command=paste)
+
+help_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label = "Help", menu=help_menu)
+help_menu.add_command(label="About", command=about)
+
+
 
 window.mainloop()
---
+++ 99-Text_File.py
@@ -0,0 +1,4 @@
+print("Hello World")
+name = input("What is your name?: ")
+print("Hello "+name)
+

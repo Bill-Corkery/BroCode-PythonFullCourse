93b37f32a68247a94fa465dd017a2e2759a862b5
Bill-Corkery <wbcorkery@gmail.com>
2023-07-11 23:42:22 -0400

    100-Tic Tac Toe Game. Making updates

--- 100-Tic Tac Toe Game.py
+++ 100-Tic Tac Toe Game.py
@@ -4,9 +4,9 @@
 from tkinter import *
 import random
 
-def next_turn():
+def next_turn(row, column):
     global player
-    if buttons[row][column]['text'] =="" and check_winner=False:
+    if buttons[row][column]['text'] == "" and check_winner() is False:
         if player == players[0]:
             buttons[row][column]['text'] = player
             if check_winner() is False:
@@ -14,21 +14,35 @@ def next_turn():
                 label.config(text = (players[1]+" turn"))
             elif check_winner() is True:
                 label.config(text = (players[0]+ " wins"))
+            elif check_winner() == "Tie":
+                label.config(text =("Tie!"))
+        else:
+            buttons[row][column]['text'] = player
+            if check_winner() is False:
+                player = players[0]
+                label.config(text = (players[0]+" turn"))
+            elif check_winner() is True:
+                label.config(text = (players[1]+ " wins"))
             elif check_winner() == 'Tie':
                 label.config(text =("Tie!"))
-    else:
-        buttons[row][column]['text'] = player
-        if check_winner() is False:
-            player = players[0]
-            label.config(text = (players[0]+" turn"))
-        elif check_winner() is True:
-            label.config(text = (players[1]+ " wins"))
-        elif check_winner() == 'Tie':
-            label.config(text =("Tie!"))
 
 def check_winner():
-    pass
-
+    #need to check all horizontal win conditions with for loop
+    for row in range(3):
+        if buttons[row][0]['text'] == buttons[row][1]['text']== buttons[row][2]['text'] != "": 
+            return True
+    for column in range(3):
+        if buttons[0][column]['text'] == buttons[1][column]['text']== buttons[2][column]['text'] != "": 
+            return True
+    if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] != "":
+        return True
+    elif buttons[0][2]['text'] == buttons[1][1]['text'] == buttons[2][0]['text'] != "":
+        return True
+    elif empty_spaces() is False:
+        return "Tie"
+    else:
+        return False
+    
 def empty_spaces():
     pass
 
@@ -37,11 +51,9 @@ def new_game():
 
 window = Tk()
 window.title("Tic-Tac-Toe")
-players = ["X", "O"]
+players = ["x", "o"]
 player = random.choice(players)
-buttons = [[0,0,0], 
-           [0,0,0], 
-           [0,0,0]]
+buttons = [[0,0,0], [0,0,0], [0,0,0]]
 
 label = Label(text=player + " turn", font=("consolas",40))
 label.pack(side="top")
